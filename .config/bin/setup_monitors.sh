5bf05140d3d5e0651a814fdbb090126b62ff2ce8
thephenrique <phenrique.silva@protonmail.com>
2024-05-23 02:26:08 -0300

    fix: fix slow framerate on 4k monitor - notebook without GPU :)

--- .config/bin/setup_monitors.sh
+++ .config/bin/setup_monitors.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
-# Set primary monitor.
-xrandr --output "HDMI-1" --primary --left-of "eDP-1" --scale 1x1
-sleep 3
+# The single monitor.
+xrandr \
+  --output "HDMI-1" --primary --left-of "eDP-1" --scale 1x1 \
+  --output "eDP-1" --off \
+  --output "DP-1" --off \
+  --output "DP-2" --off
+sleep 2
 
-# Set resolution and DPI on primary monitor.
-xrandr --output "HDMI-1" -s 3840x2160
-sleep 3
+xrandr -s 2560x1440
+sleep 2
+
 xrandr --dpi 144
-# xrandr --output "HDMI-1" --dpi 144
-sleep 3
-
-# Set resolution and DPI on notebook monitor.
-xrandr --output "eDP-1" -s 1920x1080
-sleep 3
-# xrandr --output "eDP-1" --dpi 96
-sleep 3
-
-# Disable USB-C monitors.
-xrandr --output "DP-1" --off
-sleep 3
-xrandr --output "DP-2" --off
-sleep 3
+sleep 2
 
 i3-msg restart
