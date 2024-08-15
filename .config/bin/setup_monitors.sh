14edb2f4d9b6d6d193a602584a9e73f987df2ca6
thephenrique <phenrique.silva@protonmail.com>
2024-08-15 19:32:46 -0300

    feat: enable 4k
    * It was a bit slow on a notebook without a dedicated video place.
    * But the quality is incredible.

--- .config/bin/setup_monitors.sh
+++ .config/bin/setup_monitors.sh
@@ -8,7 +8,8 @@ xrandr \
   --output "DP-2" --off
 sleep 2
 
-xrandr -s 2560x1440
+xrandr -s 3840x2160
+# xrandr -s 2560x1440
 sleep 2
 
 # 96 Small (default)
@@ -16,7 +17,7 @@ sleep 2
 # 144 Larger 150%
 # 192 Extra Large 200%
 # 240 Custom 250%
-xrandr --dpi 120
+xrandr --dpi 144
 sleep 2
 
 i3-msg restart
