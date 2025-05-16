8331a0c242251dcc21fbda18fb82a9c1ad1179db
Pedro Henrique <phsilva.net@gmail.com>
2025-05-16 19:17:06 -0300

    feat(i3): improve monitors handling

--- .config/bin/setup_monitors.sh
+++ .config/bin/setup_monitors.sh
@@ -1,27 +1,51 @@
 #!/bin/bash
 
-# The single monitor.
-# xrandr --output "DP-1" --primary --left-of "eDP-1" --scale 1x1 --output "eDP-1" --off --output "DP-1" --off
-xrandr --output "HDMI-1" --primary --left-of "eDP-1" --scale 1x1 --output "eDP-1" --off --output "DP-1" --off
-# xrandr \
-#   --output "HDMI-1" --primary --left-of "eDP-1" --scale 1x1 \
-#   --output "eDP-1" --off
-# --output "DP-1" --off \
-# --output "DP-2" --off
-
-sleep 2
-
-xrandr -s 3840x2160
-# xrandr -s 2560x1440
-sleep 2
-
+# DPI
+#
 # 96 Small (default)
 # 120 Medium 125% * 2k
 # 144 Larger 150% * 4k
 # 192 Extra Large 200%
 # 240 Custom 250%
-xrandr --dpi 144
+#
 # xrandr --dpi 120
-sleep 2
 
-i3-msg restart
+MONITOR=$1
+
+turnOnNotebookMonitor() {
+  xrandr --output "eDP-1" --auto --primary --scale 1x1 --output "HDMI-1" --off --output "DP-1" --off
+  sleep 2
+
+  xrandr -s 1920x1080
+  sleep 2
+
+  xrandr --dpi 96
+  sleep 2
+
+  i3-msg restart
+}
+
+turnOnHDMI1Monitor() {
+  xrandr --output "HDMI-1" --auto --primary --left-of "eDP-1" --scale 1x1 --output "eDP-1" --off --output "DP-1" --off
+  sleep 2
+
+  xrandr -s 3840x2160
+  sleep 2
+
+  xrandr --dpi 144
+  sleep 2
+
+  i3-msg restart
+}
+
+if [[ "$MONITOR" == "notebook" ]]; then
+  turnOnNotebookMonitor
+  exit 0
+fi
+
+if [[ "$MONITOR" == "hdmi-1" ]]; then
+  turnOnHDMI1Monitor
+  exit 0
+fi
+
+echo "Please, specify the monitor: notebook, hdmi-1."
