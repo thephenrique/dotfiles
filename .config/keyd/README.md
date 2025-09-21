40c408ba2f829676a2e2689dd5147d3c3f52f7fb
Pedro Henrique <phsilva.net@gmail.com>
2025-09-21 04:52:14 -0300

    feat: varios improvements

---
+++ .config/keyd/README.md
@@ -0,0 +1,5 @@
+`keyd` should find this file in `/etc/keyd/default.conf`.
+
+```sh
+sudo ln -s ~/.config/keyd/default.conf /etc/keyd/default.conf
+```
--- .config/waybar/scripts/backlight.sh
+++ .config/waybar/scripts/backlight.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 case $1 in
-	'up') brightnessctl -n set 1%+ ;;
-	'down') brightnessctl -n set 1%- ;;
+  'up') brightnessctl -n set 1%+ ;;
+  'down') brightnessctl -n set 1%- ;;
 esac
 
 level=$(brightnessctl -m | awk -F',' '{print $4}')
