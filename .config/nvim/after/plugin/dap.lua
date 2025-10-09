e58206409ced93f33f1edb6ee062d50d206f5c59
Pedro Henrique <phsilva.net@gmail.com>
2025-10-08 21:08:23 -0300

    feat: changes to dap

--- .config/nvim/after/plugin/dap.lua
+++ .config/nvim/after/plugin/dap.lua
@@ -53,30 +53,34 @@ Mappings for DAP.
 
 --]]
 
--- How To Debug Go Application.
+-- How To Debug Go HTTP Application.
 --
 -- Allow ptrace temporarily.
 -- 1. echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
--- 2. Build Go application with the Debugging Information flags enabled.
--- 3. Run application.
--- 4. Set a breakpoint.
--- 5. <F2> - Attach to running application.
+-- Setup environment.
+-- 1. Build Go application with the Debugging Information flags: `-gcflags="all=-N -l"`.
+-- 2. Run the binary application.
+-- 3. Set a breakpoint in the first function that will be called, for example, the handler/controller (not the route/router) of an endpoint.
+-- 4. <F2> - "5. Attach" - Attach to the running application. Search by name, for example, "bin/app".
+-- 5. Done! Call the endpoint.
 --
--- How To Debug Go Tests (from the cursor).
+-- How To Debug Go Tests (from the cursor position).
 --
--- 1. Set a breakpoint.
--- 2. <F2> - "7. Debug test (go.mod)"
+-- 1. Position the cursor on the test function that will be called.
+-- 2. Set a breakpoint.
+-- 3. <F2> - "7. Debug test (go.mod)"
 
+dap.set_log_level("TRACE")
 vim.keymap.set("n", "<F1>", dap.toggle_breakpoint)
 vim.keymap.set("n", "<F2>", dap.continue)
 vim.keymap.set("n", "<F3>", dap.step_into)
 vim.keymap.set("n", "<F4>", dap.step_over)
 vim.keymap.set("n", "<F5>", dap.step_out)
+vim.keymap.set("n", "<F6>", dap.run_to_cursor)
 vim.keymap.set("n", "<F11>", dap.run_last)
 vim.keymap.set("n", "<F12>", dap.restart)
--- vim.keymap.set("n", "<leader>gb", dap.run_to_cursor)
 
-vim.keymap.set("n", "<space>?", function()
+vim.keymap.set("n", "<leader>?", function()
 	---@diagnostic disable-next-line: missing-fields
 	dapui.eval(nil, { enter = true })
 end)
---
+++ .config/nvim/ftplugin/c.lua
@@ -0,0 +1,4 @@
+-- Identation, tabs and spaces.
+vim.opt.tabstop = 4 -- 8 (default)
+vim.opt.shiftwidth = 4 -- 8 (default)
+vim.opt.softtabstop = 4 -- 8 (default)
